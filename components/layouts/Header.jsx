e10b512424ff80f1c0b0876847506905ea2d466d
정수진 <sssjsjj@ipartners.co.kr>
2023-03-12 19:29:49 +0900

    rename: Header scss 경로 변경

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import styles from './Header.module.scss'
+import styles from '@/styles/components/layouts/Header.module.scss'
 
 export default function Header() {
   const { header, logo, utils, util } = styles
