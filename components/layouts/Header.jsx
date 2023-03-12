e286b3bcc0c4cab983a0c9d9f95a202231eedaad
정수진 <sssjsjj@ipartners.co.kr>
2023-03-12 19:10:29 +0900

    design: Header UI 작업

---
+++ components/layouts/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import styles from './Header.module.scss'
+
+export default function Header() {
+  const { header, logo, utils, util } = styles
+  return (
+    <div className={header}>
+      <h1 className={logo}>
+        <Link href="/">📑blog</Link>
+      </h1>
+      <ul className={utils}>
+        <li className={util}>
+          <button>회원 가입</button>
+        </li>
+        <li className={util}>
+          <button>로그인</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
