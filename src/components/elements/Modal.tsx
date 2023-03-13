0679d4e71f37bd5e1b7d90cba20a4b63eb3bcff8
정수진 <sssjsjj@ipartners.co.kr>
2023-03-13 22:07:22 +0900

    refactor: Modal 컴포넌트 css module에서 styled component로 변경

--- src/components/elements/Modal.tsx
+++ src/components/elements/Modal.tsx
@@ -1,5 +1,5 @@
-import styles from '@/styles/components/elements/Modal.module.scss'
-import { ReactNode } from 'react';
+import styled from 'styled-components'
+import { ReactNode } from 'react'
 
 type ModalProps = {
   isOpen: boolean,
@@ -8,24 +8,77 @@ type ModalProps = {
   children: ReactNode
 };
 
-export default function Modal (props: ModalProps) {
+function Modal (props: ModalProps) {
   const { isOpen, title, toggle, children } = props
-  const { wrapper, modal, wrapTitle, modalTitle, wrapContent, buttonClose } = styles
+  
   return (
     <>
       {isOpen && (
-        <section className={wrapper} onClick={toggle}>
-          <div onClick={(e) => e.stopPropagation()} className={modal}>
-            <div className={wrapTitle}>
-              <h2 className={modalTitle}>{title}</h2>
-            </div>
-            <div className={wrapContent}>{children}</div>
-            <button className={buttonClose} onClick={toggle}>
+        <Wrapper onClick={toggle}>
+          <ModalBox onClick={(e) => e.stopPropagation()}>
+            <WrapTitle>
+              <Title>{title}</Title>
+            </WrapTitle>
+            <WrapContent>{children}</WrapContent>
+            <ButtonClose onClick={toggle}>
               닫기
-            </button>
-          </div>
-        </section>
+            </ButtonClose>
+          </ModalBox>
+        </Wrapper>
       )}
     </>
   )
 }
+
+const modalPd = {
+  vtc: '40px',
+  hrz: '30px'
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  background-color: var(--bg-dimmed);
+`
+
+const ModalBox = styled.section`
+  position: relative;
+  min-width: 400px;
+  padding: ${modalPd.vtc} ${modalPd.hrz};
+  background-color: var(--bg-white);
+  transform: translateY(calc(25% - 20vh));
+  border-radius: var(--border-radius-M);
+  box-shadow: var(--shadow-M);
+`
+
+const WrapTitle = styled.div`
+  text-align: center;
+  margin-bottom: 40px;
+`
+
+const Title = styled.h2`
+  font-size: var(--font-size-title-M);
+  font-weight: var(--font-weight-X-bold);
+  line-height: 1.2;
+`
+
+const WrapContent = styled.div`
+  font-size: var(--font-size-title-M);
+  font-weight: var(--font-weight-X-bold);
+  line-height: 1.2;
+`
+
+const ButtonClose = styled.button`
+  position: absolute;
+  top: ${modalPd.vtc};
+  right: ${modalPd.hrz};
+`
+
+export default Modal
