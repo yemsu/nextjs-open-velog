4106b816333f573f960b13730a90dd14409cac4c
정수진 <sssjsjj@ipartners.co.kr>
2023-03-13 18:13:28 +0900

    style: styled-reset 설치 및 styled-components로 global style 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import GlobalStyle from '@/styles/global-style'
 import Header from '@/components/layouts/Header'
 import '@/styles/globals/index.scss'
 import type { AppProps } from 'next/app'
@@ -5,6 +6,7 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Component {...pageProps} />
     </>
---
+++ src/styles/global-style.ts
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export default createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: var(--font-family);
+    font-size: var(--font-size-M-pc);
+    font-weight: var(--font-weight-normal);
+    color: var(--font-black);
+    line-height: 1.4;
+    letter-spacing: -0.025em;
+  }
+  button {
+    appearance: none;
+    border: none;
+    background-color: transparent;
+    color: inherit;
+    letter-spacing: inherit;
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+    letter-spacing: inherit;
+  }
+  button, a {
+    &:hover {
+      opacity: .7;
+      transition: opacity .2s
+    }
+  }
+  strong {
+    font-weight: var(--text-weight-X-bold);
+  }
+  input[type="radio"],
+  input[type="checkbox"] {
+    margin: 0;
+  }
+  // only pc, mobile
+  .only-pc {
+    @include mobile {
+      display: none !important;
+    }
+  }
+  .only-mobile {
+    @include pc {
+      display: none !important;
+    }
+  }
+  // ir
+  .ir-hidden {
+    position: absolute;
+    overflow: hidden;
+    width: 1px;
+    height: 1px;
+    font-size: 1px;
+  }
+`;
