90274bf84a41641ad103ecfe7542efd9521d7fe2
정수진 <sssjsjj@ipartners.co.kr>
2023-03-15 22:57:10 +0900

    feat: path, etc constants 생성

--- src/constants/etc.ts
+++ src/constants/etc.ts
@@ -1,1 +1,2 @@
 export const AUTH_TOKEN = 'AUTH_TOKEN'
+export const USER_INFO = 'USER_INFO'
---
+++ src/constants/path.ts
@@ -0,0 +1,14 @@
+export const SERVICES = {
+  MEMBER: 'members',
+  BLOG: 'blogs',
+  BOARD: 'boards',
+  BOARD_WISH: 'board/wished',
+  RANK: 'rank/keyword',
+  KEYWORD: 'keywords'
+}
+
+export const PAGES = {
+  MAIN: '/',
+  BLOG: '/blog',
+  MY_BLOG: '/my/blog',
+}
