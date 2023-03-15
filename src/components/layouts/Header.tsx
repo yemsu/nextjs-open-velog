fb6c38208b2fcaf7c19ca2cb3f2adabbd8305da6
정수진 <sssjsjj@ipartners.co.kr>
2023-03-15 22:24:01 +0900

    design: header contents wrapper 가로 사이즈 변경

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,23 +1,29 @@
 import Link from "next/link"
 import { useSelector, useDispatch  } from "react-redux";
-import { getIsLogin, SET_IS_LOGIN } from "@/store/auth";
+import { getIsLogin, getUserInfo, SET_IS_LOGIN, SET_USER_INFO } from "@/store/auth";
 import Axios from "@/api/Axios";
-import { AUTH_TOKEN } from "@/constants/etc";
+import { AUTH_TOKEN, USER_INFO } from "@/constants/etc";
 import JoinModal from "@/components/auth/JoinModal"
 import ContentWrapper from "@/components/layouts/ContentWrapper"
 import useModal from "@/hooks/useModal"
 import styled from "styled-components"
 import { useEffect, useState } from "react"
+import { PAGES } from "@/constants/path";
 
 export default function Header() {
   const [isOpen, toggle] = useModal()
   const [joinOrLogin, setJoinOrLogin] = useState('join')
   const dispatch = useDispatch()
   const isLogin = useSelector(getIsLogin);
+  const userInfo = useSelector(getUserInfo);
 
   useEffect(() => {
     const hasToken = localStorage.getItem(AUTH_TOKEN)
-    if(hasToken) dispatch(SET_IS_LOGIN(true))
+    const userInfoStr = localStorage.getItem(USER_INFO)
+    if(hasToken && userInfoStr) {
+      dispatch(SET_IS_LOGIN(true))
+      dispatch(SET_USER_INFO(JSON.parse(userInfoStr)))
+    }
   }, [])
 
   const onClickUtil = (type: string) => {
@@ -27,14 +33,16 @@ export default function Header() {
 
   const onClickLogout = () => {
     localStorage.removeItem(AUTH_TOKEN)
+    localStorage.removeItem(USER_INFO)
     dispatch(SET_IS_LOGIN(false))
+    dispatch(SET_USER_INFO(null))
     Axios.prototype.authToken = null
   }
   
   return (
     <>
       <Wrapper>
-        <ContentWrapper layoutType="flex-row">
+        <ContentWrapper size="full" layoutType="flex-row">
           <Logo>
             <Link href="/">Open Velog</Link>
           </Logo>
@@ -50,7 +58,7 @@ export default function Header() {
               </> :
               <>
                 <Util>
-                  <UtilLink href="/boards">My Velog</UtilLink>
+                  {userInfo && <UtilLink href={`${PAGES.BLOG}/${userInfo.userId}`}>My Velog</UtilLink>}
                 </Util>
                 <Util>
                   <UtilButton onClick={onClickLogout}>로그아웃</UtilButton>
@@ -71,7 +79,6 @@ export default function Header() {
 
 const Wrapper = styled.div`
   height: 55px;
-  padding: 0 30px;
 `
 
 const Logo = styled.h1`
