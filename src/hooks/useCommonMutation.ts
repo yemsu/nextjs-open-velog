b3216f7f92df0225f7b37c22862e50cdaed0d81b
정수진 <sssjsjj@gmail.com>
2023-03-17 17:48:56 +0900

    feat: useCommonMutation 커스텀 훅 신규 작업

---
+++ src/hooks/useCommonMutation.ts
@@ -0,0 +1,30 @@
+import { useMutation } from "react-query"
+
+interface Options<Response, Payload, TError> {
+  onSuccess?: () => void;
+  onError?: () => void;
+  onSettled?: () => void;
+}
+
+function useCommonMutation<Response, Payload, TError = unknown>(
+  mutationFn: (payload: Payload) => Promise<Response>,
+  options?: Options<Response, Payload, TError>
+) {
+  const { onSuccess, onError, onSettled } = options || {};
+  return useMutation(
+    mutationFn,
+    {
+      onSuccess: () => {
+        onSuccess && onSuccess();
+      },
+      onError: () => {
+        onError && onError();
+      },
+      onSettled: () => {
+        onSettled && onSettled();
+      }
+    }
+  )
+}
+
+export default useCommonMutation
