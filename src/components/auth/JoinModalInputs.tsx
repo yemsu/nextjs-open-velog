d53293873b662fcde5320a91fedbde719725f7d4
정수진 <sssjsjj@ipartners.co.kr>
2023-03-21 21:31:49 +0900

    fix: 생년월일 placeholder 수정

--- src/components/auth/JoinModalInputs.tsx
+++ src/components/auth/JoinModalInputs.tsx
@@ -66,7 +66,7 @@ const inputCategories: InputCategory[][] = [
     type: 'number',
     name: 'birthday',
     label: '생년월일',
-    placeholder: '생년월일 8자리 ex)19910610',
+    placeholder: '생년월일 8자리 ex)yyyymmdd',
     reg: /^[0-9]{8}$/,
   }],
   [{
