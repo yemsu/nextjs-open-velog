d6968c419f68228ce3da3934cf0f672bef49953d
정수진 <sssjsjj@gmail.com>
2023-03-22 17:21:31 +0900

    refactor: InputList 컴포넌트 분리 신규 생성

---
+++ src/components/elements/InputList.tsx
@@ -0,0 +1,78 @@
+import styled from "styled-components"
+import { ValidationsState } from "@/types/auth"
+import { InputCategory } from "@/types/form"
+import React, { SyntheticEvent } from "react"
+import Input from "./Input"
+import InputWrapper from "./InputWrapper"
+
+interface InputListProps {
+  inputList: InputCategory[]
+  forms: {[key: string]: string}
+  onChange: (e: SyntheticEvent) => void
+  validations?: ValidationsState
+}
+
+function InputList(props: InputListProps) {
+  const {
+    inputList,
+    forms,
+    onChange,
+    validations
+  } = props
+
+  return (
+    <InputWrapper>
+      {inputList.map(({
+        type,
+        id,
+        name,
+        label,
+        value,
+        placeholder
+      }: InputCategory, i) => {
+        return (
+          <React.Fragment key={name + id}>
+            <Input
+              type={type}
+              id={id}
+              name={name}
+              label={label}
+              value={value || forms[name]}
+              checked={forms[name] === value}
+              placeholder={placeholder}
+              onChange={onChange}
+            />
+            {
+              validations && validations[name]?.text &&
+              <TextValidation className={validations[name].isValid ? 'pass' : ''}>
+                {validations[name].text}
+              </TextValidation>
+            }
+          </React.Fragment>
+        )
+      })}
+    </InputWrapper>
+  )
+}
+
+const TextValidation = styled.p`
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: var(--font-size-XS);
+  &:not(.pass) {
+    color: #f33c5a;
+    font-weight: var(--font-weight-bold);
+    &:before {
+      content: '❗';
+    }
+  }
+  &.pass {
+    color: #21b98c;
+    &:before {
+      content: '✅';
+    }
+  }
+`
+
+export default InputList
